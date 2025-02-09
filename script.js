cad11250c0a36df9292999f4c4b8793def06e6d9
Sufiyan Munshi <101869552+sufiyan02@users.noreply.github.com>
2025-02-09 20:30:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 function stringChop(str, size) {
   // your code here
-}
+	   if (!str) return []; // Return empty array if input is null or empty
 
+    let result = [];
+    for (let i = 0; i < str.length; i += size) {
+        result.push(str.substring(i, i + size));
+    }
+    
+    return result;
+}
 // Do not change the code below
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
